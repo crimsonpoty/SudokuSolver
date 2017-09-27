729ebe5e74a3279e256b3f83215079ef6959a4f0
crimson_88 <crimson_88@naver.com>
2017-09-27 15:20:18 +0900

    C++ 알고리즘 변경에 따른 수정사항 반영

--- python/main/cpp/make_cppSudoku.bat
+++ python/main/cpp/make_cppSudoku.bat
@@ -4,18 +4,19 @@ REM replace user_name to your account name at "PY_INCLUDE_PATH" and "PY_LIB_PATH
 set SRC_PATH="..\..\..\c++"
 set OBJ_PATH="%CD%\src"
 set INCLUDE_PATH=%OBJ_PATH%
-set PY_INCLUDE_PATH="C:\Users\crims\AppData\Local\Programs\Python\Python36-32\include"
-set PY_LIB_PATH="C:\Users\crims\AppData\Local\Programs\Python\Python36-32\libs"
+set PY_INCLUDE_PATH="C:\Users\user_name\AppData\Local\Programs\Python\Python36-32\include"
+set PY_LIB_PATH="C:\Users\user_name\AppData\Local\Programs\Python\Python36-32\libs"
 
 if not exist "src" (mkdir "src")
 
+copy %SRC_PATH%"\MyDebug.h" src
 copy %SRC_PATH%"\Sudoku.h" src
 copy %SRC_PATH%"\Sudoku.cpp" src
 
-g++ -std=c++0x -O0 -g3 -Wall -c -fmessage-length=0 -o %OBJ_PATH%"\Sudoku.o" %OBJ_PATH%"\Sudoku.cpp" 
+g++ -std=c++11 -O2 -Wall -c -fmessage-length=0 -o %OBJ_PATH%"\Sudoku.o" %OBJ_PATH%"\Sudoku.cpp" 
 
 swig -c++ -python Sudoku.i
 
 REM [pyd lib file for Windows]
-g++ -c Sudoku_wrap.cxx -fPIC -Wall -I%PY_INCLUDE_PATH% -I%INCLUDE_PATH%
+g++ -std=c++11 -O2 -Wall -c Sudoku_wrap.cxx -fPIC -Wall -I%PY_INCLUDE_PATH% -I%INCLUDE_PATH%
 g++ -o _cppSudoku.pyd %OBJ_PATH%"\Sudoku.o" Sudoku_wrap.o -Wall -shared -lpython36 -L%PY_LIB_PATH%
